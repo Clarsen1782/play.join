1f243c2b467b939d1f7ff3150aef22bd687bdd4c
nathangero <nathangero@yahoo.com>
2023-11-07 15:47:01 -0800

    implement adding session and handlebars middleware

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 const path = require("path");
 const sequelize = require("./config/connection");
 const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+require("dotenv").config();
+
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: parseInt(process.env.COOKIE_AGE),
+        secure: false,
+        sameSite: 'strict',
+    },
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+}
 
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+// app.set('trust proxy', 1);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 
 sequelize.sync().then(() => {
