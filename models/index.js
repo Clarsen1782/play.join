b6de40aad9780aca33d3cf65ef03379c94de4b71
Christina Larsen <clarsen1782@outlook.com>
2023-11-08 11:35:22 -0800

    Added Index.js to Model

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const Game = require('./Game');
+const GamerTag = require('./GamerTag');
+const Platform = require('./Platform');
+const User = require('./User');
+const UserGame = require('./UserGame');
+
+module.exports = {Game, GamerTag, Platform, User, UserGame};
