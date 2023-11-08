d856a1cf873c61b65e15c2222c629d06f6d6f7cb
nathangero <nathangero@yahoo.com>
2023-11-08 12:28:39 -0800

    create relationships between models

--- models/index.js
+++ models/index.js
@@ -5,8 +5,34 @@ const Platform = require('./Platform');
 const User = require('./User');
 const UserGame = require('./UserGame');
 
+User.hasMany(Game, {
+    through: UserGame
+});
+
 User.hasMany(GamerTag, {
     foreignKey: "user_id"
 });
+
+// Connecting friends together
+User.belongsToMany(User, {
+    through: Friends,
+});
+
+// Connecting users to games
+Game.belongsToMany(User, {
+    through: UserGame
+});
+
+
+Platform.hasMany(GamerTag, {
+    foreignKey: "platform_id"
+});
+
+
+GamerTag.belongsTo(Platform, {
+    foreignKey: "platform_id"
+});
+
+
 
 module.exports = { Friends, Game, GamerTag, Platform, User, UserGame };
