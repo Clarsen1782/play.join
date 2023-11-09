847a0788f8095ac77da754074f0eba56d5fc3f5b
Christina Larsen <clarsen1782@outlook.com>
2023-11-09 10:12:12 -0800

    update

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -9,27 +9,30 @@ const userData = require('./userData.json');
 const userGameData = require('./userGameData.json');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true }); // Makes tables reinitialize ({ Makes tables update})
+    await sequelize.sync({force: true});
+    console.log("\n\n");
 
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true, //must have to make hooks work
-    returning: true,
-  });
 
-  // seeds need all models to be defined so we know what to set up
+    await User.bulkCreate(userData, { individualHooks: true }); // Runs beforeCreate for all
+    console.log("---- SEEDED USER ----\n\n");
 
-  // .bulkCreate can be used for tables that are not associated to another table (ex users, shown above)
+    await Game.bulkCreate(gameData);
+    console.log("---- SEEDED GAME ----\n\n");
 
-  // tables that need to be linked will need a for/of loop such as the example below from 28-Stu_Mini-Project with changes to suit needs
+    await Platform.bulkCreate(platformData);
+    console.log("---- SEEDED PLATFORM ----\n\n");
 
-  /* for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }*/
+    await Friends.bulkCreate(friendsData);
+    console.log("---- SEEDED FRIENDS ----\n\n");
 
-  process.exit(0);
-};
+    await GamerTag.bulkCreate(gamerTagData);
+    console.log("---- SEEDED GAMERTAG ----\n\n");
+
+    await UserGame.bulkCreate(userGameData);
+    console.log("---- SEEDED FRIENDS ----\n\n");
+
+
+    process.exit(0);
+}
 
 seedDatabase();
