904d46b78ebfe34aa4fd68329d9055591399e3b9
nathangero <nathangero@yahoo.com>
2023-11-08 17:28:01 -0800

    implement getting a user's friends; todo: join them as an array

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -25,6 +25,42 @@ router.get("/", async (req, res) => {
     } catch (error) {
         res.status(500).json(error);
     }
-})
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const data = await User.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    through: Friends,
+                    as: "friender",
+                    foreignKey: "user_id",
+                    attributes: [
+                        "id",
+                        "userName",
+                    ]
+                },
+                {
+                    model: User,
+                    through: Friends,
+                    as: "friended",
+                    foreignKey: "friend_id",
+                    attributes: [
+                        "id",
+                        "userName",
+                    ]
+                }
+            ],
+        });
+
+        const users = data.get({ plain: true });
+        
+        res.status(200).json(users);
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
