48e5c6c7fce1f59e8765411aca6149c8353734bf
nathangero <nathangero@yahoo.com>
2023-11-09 00:33:59 -0800

    implement creating "friends" array from join table

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,36 +1,24 @@
 const router = require("express").Router();
-const { User, GamerTag, Game, Friends } = require("../../models");
+const sequelize = require("../../config/connection");
+const { User, GamerTag, Game, Friends, UserGame } = require("../../models");
 
+// Get all users and all their information
 router.get("/", async (req, res) => {
     try {
         const data = await User.findAll({
             include: [
                 {
                     model: GamerTag,
+                    attributes: {
+                        exclude: [
+                            "user_id",
+                            "platform_id"
+                        ]
+                    }
                 },
                 {
                     model: Game
                 },
-                // {
-                //     model: User,
-                //     through: Friends
-                // }
-            ]
-        });
-
-        const users = data.map((element) => element.get({ plain: true }));
-        
-        res.status(200).json(users);
-
-    } catch (error) {
-        res.status(500).json(error);
-    }
-});
-
-router.get("/:id", async (req, res) => {
-    try {
-        const data = await User.findByPk(req.params.id, {
-            include: [
                 {
                     model: User,
                     through: Friends,
@@ -52,11 +40,66 @@ router.get("/:id", async (req, res) => {
                     ]
                 }
             ],
+            attributes: [
+                "id",
+                "userName"
+            ]
         });
 
-        const users = data.get({ plain: true });
-        //TODO: Join the friender and friended arrays as one
+        const users = data.map((element) => element.get({ plain: true }));
+        
         res.status(200).json(users);
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.get("/:id/friends", async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const data = await User.findByPk(userId, {
+            include: [
+                {
+                    model: User,
+                    through: Friends,
+                    as: "friender",
+                    attributes: [
+                        "id",
+                        "userName",
+                    ]
+                },
+                {
+                    model: User,
+                    through: Friends,
+                    as: "friended",
+                    attributes: [
+                        "id",
+                        "userName",
+                    ]
+                }
+            ],
+            attributes: [
+                "id",
+                "userName"
+            ]
+        });
+
+        const user = data.get({ plain: true });
+
+        const frienderList = user.friender;
+        const friendedList = user.friended;
+        const friendsList = friendedList.concat(frienderList);
+
+        // Remove these lists after making friends list
+        delete user.friender;
+        delete user.friended;
+
+        user["friends"] = friendsList;
+        console.log("user:", user);
+
+        res.status(200).json(user);
 
     } catch (error) {
         res.status(500).json(error);
