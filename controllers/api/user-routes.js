fdfd161d9a815c5c1590b0c29a848f8ba26a7e68
nathangero <nathangero@yahoo.com>
2023-11-09 00:46:56 -0800

    implement getting a user's info given an id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -53,8 +53,77 @@ router.get("/", async (req, res) => {
     } catch (error) {
         res.status(500).json(error);
     }
-});
+}); 
 
+
+// Get a user and all their info
+router.get("/:id", async (req, res) => {
+    // If 0 then it's the logged in user's profile, else it's someone else's
+    let userId = req.params.id === 0 ? req.session.userId : req.params.id;
+
+    try {
+        const data = await User.findByPk(userId, {
+            include: [
+                {
+                    model: GamerTag,
+                    attributes: {
+                        exclude: [
+                            "user_id",
+                            "platform_id"
+                        ]
+                    }
+                },
+                {
+                    model: Game
+                },
+                {
+                    model: User,
+                    through: Friends,
+                    as: "friender",
+                    attributes: [
+                        "id",
+                        "userName",
+                    ]
+                },
+                {
+                    model: User,
+                    through: Friends,
+                    as: "friended",
+                    attributes: [
+                        "id",
+                        "userName",
+                    ]
+                }
+            ],
+            attributes: [
+                "id",
+                "userName"
+            ]
+        });
+
+        const user = data.get({ plain: true });
+
+        // Create a "friends" list array from the Friends join table
+        const frienderList = user.friender;
+        const friendedList = user.friended;
+        const friendsList = friendedList.concat(frienderList);
+        user["friends"] = friendsList;
+
+        // Remove these lists after making friends list
+        delete user.friender;
+        delete user.friended;
+
+        console.log("user:", user);
+
+        res.status(200).json(user);
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}); 
+
+
+// Get a user's friends
 router.get("/:id/friends", async (req, res) => {
     const userId = req.params.id;
 
