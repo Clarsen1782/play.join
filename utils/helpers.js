748a076bd69d44a5fd02b1278e6ff794fb0e70f9
nathangero <nathangero@yahoo.com>
2023-11-11 02:34:48 -0800

    implement Accept Friend Request button

--- utils/helpers.js
+++ utils/helpers.js
@@ -9,12 +9,22 @@ module.exports = {
         for (const friend of userToFriend.friends) {
             // Checks if the user being viewed IS friends with the user logged in
             if (friend.id === loggedInId && friend.isFriend) {
-                // console.log("is a friend");
+                console.log("is a friend");
                 return false;
             }
         }
 
         // User can be added as a friend
+        console.log("not a friend yet")
         return true;
+    },
+    isFriender(userToFriend, loggedInId) {
+        for (const friend of userToFriend.friends) {
+            
+            if (friend.id === loggedInId && !friend.isFriend) {
+                // Check if the user we're viewing sent or received the friend request to the logged in user
+                return friend.isFriender;
+            }
+        }
     }
 }
